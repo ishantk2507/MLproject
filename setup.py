b905b07c11ce403bf318e1f4357d3726c0b9b530
ishcant <ishantk250705@gmail.com>
2025-12-29 21:58:15 +0530

    setup update

--- setup.py
+++ setup.py
@@ -7,12 +7,13 @@ def get_requirements(file_path:str)->List[str]:
     This function will return the list of requirements
     '''
     requirements = []
+    HYPEN_E_DOT = "-e ."
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements ]
 
-        if "-e ." in requirements:
-            requirements.remove("-e .")
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
